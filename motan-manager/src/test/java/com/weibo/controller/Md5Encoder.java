9fb49c3ec02aa64f552223e464dac693a5d797cd
高科 <kegao@creditease.cn>
2017-01-04 19:34:44 +0800

    update manager password

---
+++ motan-manager/src/test/java/com/weibo/controller/Md5Encoder.java
@@ -0,0 +1,16 @@
+package com.weibo.controller;
+
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+
+/**
+ * Created by kegao on 2017/1/4.
+ */
+public class Md5Encoder {
+    public static void main(String[] args) {
+        Md5PasswordEncoder md5PasswordEncoder = new Md5PasswordEncoder();
+        String ps = md5PasswordEncoder.encodePassword("guest123","guest");
+        System.out.println(ps);
+        ps = md5PasswordEncoder.encodePassword("admin966","admin");
+        System.out.println(ps);
+    }
+}
